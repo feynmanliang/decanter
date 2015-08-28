15598802cffac691c88cd78e7fbfdcc309bd0375
Feynman Liang <fliang@databricks.com>
2015-08-27 20:13:19 -0700

    Adds AuctionPage and routes

--- AuctionPage.jsx
+++ AuctionPage.jsx
@@ -1,8 +1,21 @@
 AuctionPage = React.createClass({
+
+  mixins: [ReactMeteorData],
+
+  propTypes: {
+    _id: React.PropTypes.string.isRequired
+  },
+
+  getMeteorData() {
+    return {
+      auction: Auctions.find(this.props._id).fetch()
+    }
+  },
   render() {
+    console.log(this.data.auction);
     return (
       <div>
-        {this.props._id}
+        {JSON.stringify(this.data.auction)}
       </div>
     );
   }
