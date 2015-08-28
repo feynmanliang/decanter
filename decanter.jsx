0fa2cf6ba9f564967a67574a7b69141e1fa65c7a
Feynman Liang <fliang@databricks.com>
2015-08-27 20:34:56 -0700

    Adds register with username and restricts bids for logged in

--- decanter.jsx
+++ decanter.jsx
@@ -1,4 +1,25 @@
 Auctions = new Mongo.Collection("auctions");
 
+var pwd = AccountsTemplates.removeField('password');
+AccountsTemplates.removeField('email');
+AccountsTemplates.addFields([
+  {
+      _id: "username",
+      type: "text",
+      displayName: "username",
+      required: true,
+      minLength: 2,
+  },
+  //{
+  //    _id: 'email',
+  //    type: 'email',
+  //    required: true,
+  //    displayName: "email",
+  //    re: /.+@(.+){2,}\.(.+){2,}/,
+  //    errStr: 'Invalid email',
+  //},
+  pwd
+]);
+
 if (Meteor.isClient) {
 }
